9701ee199356c34ed25fe5cce56602c978e2b13b
joesan <codeintheopen@gmail.com>
2019-09-03 14:31:30 +0200

    added cache for storing metadata in the actor

---
+++ src/main/scala/com/eon/opcuapubsubclient/cache/DataSetMetaDataCache.scala
@@ -0,0 +1,38 @@
+package com.eon.opcuapubsubclient.cache
+
+import akka.actor.{Actor, Props}
+import com.eon.opcuapubsubclient.cache.DataSetMetaDataCache._
+import com.eon.opcuapubsubclient.domain.PayloadTypes.DataSetMetaData
+
+/**
+  * The idea behind this actor is that we can dump messages (stored as a HashMap)
+  * into it and retrieve it based on the key
+  */
+class DataSetMetaDataCache extends Actor {
+
+  override def receive: Receive = {
+    case _ => run(Map.empty[String, DataSetMetaData])
+  }
+
+  def run(cache: Map[String, DataSetMetaData]): Receive = {
+    // Mutation messages
+    case Evict(key) =>
+      context.become(run(cache - key))
+    case Add(key, value) =>
+      context.become(run(cache + (key -> value)))
+
+    // Information retrieval messages
+    case Get(name) =>
+      cache.get(name)
+  }
+}
+object DataSetMetaDataCache {
+
+  sealed trait CacheMessage
+  case class Evict(key: String) extends CacheMessage
+  case class Add(key: String, value: DataSetMetaData) extends CacheMessage
+  case class Get(key: String) extends CacheMessage
+
+  def props(): Props =
+    Props(new DataSetMetaDataCache)
+}
